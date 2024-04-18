faccba07da4914d30fddb8ce62fad1d2323ada0e
Akeila Harris <akeila0606@gmail.com>
2024-04-18 14:49:54 -0400

    Added dependancies

--- app/src/main/java/com/example/plantology/PlantDetailActivity.java
+++ app/src/main/java/com/example/plantology/PlantDetailActivity.java
@@ -6,7 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PlantDetailActivity extends AppCompatActivity {
 
@@ -24,6 +35,5 @@ public class PlantDetailActivity extends AppCompatActivity {
         Intent intent = new Intent(PlantDetailActivity.this, HomeActivity.class);
         startActivity(intent);
     }
-
 
 }
